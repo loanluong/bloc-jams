957dfbb84d62cc39645d4309866d0a9e71864ca4
Lani Luong <loanlu@gmail.com>
2016-10-27 16:39:29 -0700

    assignment 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,31 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";              
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scale(X) translateY(0)";
+};
 
 var animatePoints = function(points) {
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";              
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scale(X) translateY(0)";
-    };
-    
-      for (var i=0; i<points.length; i++) {
-        revealPoint(i);    
-    }
+    forEach(points, revealPoint); 
 };
 
 window.onload = function() {
+     if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-        animatePoints(pointsArray);
-    }
+    
+    window.addEventListener("scroll", function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
     });
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
-    }
 }
+        
+
+   
         
